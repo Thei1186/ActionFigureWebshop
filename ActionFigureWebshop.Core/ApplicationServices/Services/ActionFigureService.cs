d700176b9c2dca7e9c0bd3cf5cabcb1d7abbbf97
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-10-07 11:01:40 +0200

    Context

--- ActionFigureWebshop.Core/ApplicationServices/Services/ActionFigureService.cs
+++ ActionFigureWebshop.Core/ApplicationServices/Services/ActionFigureService.cs
@@ -1,7 +1,33 @@
-﻿namespace ActionFigureWebshop.Core.ApplicationServices.Services
+﻿using System.Collections.Generic;
+using ActionFigureWebshop.Core.Entity;
+
+namespace ActionFigureWebshop.Core.ApplicationServices.Services
 {
-    public class ActionFigureService
+    public class ActionFigureService: IActionFigureService
     {
-        
+        public ActionFigure ReadActionFigure(ActionFigure figure)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionFigure CreateActionFigure(ActionFigure figure)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionFigure DeleteActionFigure(ActionFigure figure)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionFigure UpdateActionFigure(ActionFigure figure)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<ActionFigure> ReadAllActionFigures()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
