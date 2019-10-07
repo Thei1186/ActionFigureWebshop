a7db8c7623bf98457e73e2ae569210aefe32ee28
andreas <andreas-cooper@hotmail.com>
2019-10-07 10:35:18 +0200

    ActionFigure

--- ActionFigureWebshop.Core/DomainServices/IActionFigureRepository.cs
+++ ActionFigureWebshop.Core/DomainServices/IActionFigureRepository.cs
@@ -1,10 +1,17 @@
-﻿using ActionFigureWebshop.Core.Entity;
+﻿using System.Collections.Generic;
+using ActionFigureWebshop.Core.Entity;
 
 namespace ActionFigureWebshop.Core.DomainServices
 {
     public interface IActionFigureRepository
     {
-        void Creat(ActionFigure);
+        ActionFigure Creat(ActionFigure actionFigure);
+        List<ActionFigure> readAll();
+
+        ActionFigure GetActionFigureById(int id);
+
+        ActionFigure update(ActionFigure actionFigure);
+        ActionFigure delete(ActionFigure actionFigure);
 
     }
 }
