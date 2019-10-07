e2762fa9004c5899e9fa63ceaa8013759d246248
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-10-07 10:43:26 +0200

    Services

---
+++ ActionFigureWebshop.Core/ApplicationServices/IActionFigureService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ActionFigureWebshop.Core.Entity;
+
+namespace ActionFigureWebshop.Core.ApplicationServices
+{
+    public interface IActionFigureService
+    {
+        ActionFigure ReadActionFigure(ActionFigure figure);
+        ActionFigure CreateActionFigure(ActionFigure figure);
+        ActionFigure DeleteActionFigure(ActionFigure figure);
+        ActionFigure UpdateActionFigure(ActionFigure figure);
+        List<ActionFigure> ReadAllActionFigures();
+    }
+}
