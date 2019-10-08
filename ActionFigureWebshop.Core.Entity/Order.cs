9ead87a73c42dee4d9a1f5c4af922a5bd3c090fc
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-10-08 19:55:55 +0200

    Update Order.cs

--- ActionFigureWebshop.Core.Entity/Order.cs
+++ ActionFigureWebshop.Core.Entity/Order.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ActionFigureWebshop.Core.Entity
 {
     public class Order
     {
         public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
         public Customer OrderCustomer { get; set; }
-        public List<ActionFigure> ActionFigures { get; set; }
+        public List<OrderLine> OrderLines { get; set; }
         public double TotalOrderPrice { get; set; }
     }
 }
