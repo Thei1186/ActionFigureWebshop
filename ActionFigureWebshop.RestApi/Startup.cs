d3dce65e09842214273381c68b8c5c9cbdc425f5
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-10-08 10:21:20 +0200

    Update Startup.cs

--- ActionFigureWebshop.RestApi/Startup.cs
+++ ActionFigureWebshop.RestApi/Startup.cs
@@ -69,12 +69,13 @@ namespace ActionFigureWebshop.RestApi
                 using (var scope = app.ApplicationServices.CreateScope())
                 {
                     var context = scope.ServiceProvider.GetRequiredService<ActionFigureShopContext>();
-                    context.Database.EnsureCreated();
+                    DbInitializer.SeedDB(context);
                 }
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
